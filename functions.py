748b405fa2d992b74c01b2fdb901e571fbfd628b
Angie Méndez <almendezl@unal.edu.co>
2022-04-08 12:54:10 +0200

    performance functions

---
+++ functions.py
@@ -0,0 +1,35 @@
+# functions.py>
+
+
+# The inputs are two vectors of the same size, where y denotes the sales 
+# of a store and y_ denotes the corresponding predictions
+
+def MAPE(y, y_) :
+
+    import numpy as np
+    from math import sqrt
+    
+    y = np.array(y)
+    y_ = np.array(y_)
+    n = len(y)
+    return round((1/n)*sum((abs(y-y_)/y)), 5)
+
+def MSE(y, y_) :
+
+    import numpy as np
+    from math import sqrt
+    
+    y = np.array(y)
+    y_ = np.array(y_)
+    n = len(y)
+    return round((1/n)*sum(((y-y_))**2), 5)
+
+def MAE(y, y_) :
+
+    import numpy as np
+    from math import sqrt
+    
+    y = np.array(y)
+    y_ = np.array(y_)
+    n = len(y)
+    return round((1/n)*sum((abs(y-y_))), 5)
